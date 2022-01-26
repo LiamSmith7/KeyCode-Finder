8858e0269e311839aa1c3a08dc1d4c53bcecd6e0
LiamSmith7 <unconfigured@null.spigotmc.org>
2022-01-26 17:57:32 +0000

    Initial commit w/ basic animations

---
+++ app.js
@@ -0,0 +1,55 @@
+let keyPressed = document.getElementById("key").getElementsByTagName("h3")[0];
+let keyCode = document.getElementById("code").getElementsByTagName("h3")[0];
+let keyWhich = document.getElementById("which").getElementsByTagName("h3")[0];
+let keyMain = document.getElementById("main").getElementsByTagName("h1")[0];
+let mobileInstructions = document.getElementById("mobile_instructions");
+
+
+let mainMovement = anime({
+    targets: keyMain,
+    translateY: -25, // Moves up the page 25 pixels
+    direction: 'alternate', // Resets once the animation is complete
+    easing: 'easeInOutSine', // Slows down once it gets close to its end point
+    innerText: [0, 8940000],
+    duration: 250 // 250ms
+});
+mainMovement.pause();
+
+document.addEventListener("keydown", (keyObject) => {
+
+    // Shows everything except the mobile message on keydown
+    for(let element of document.body.getElementsByTagName("div")){ 
+        if(element.id == "mobile_instructions"){
+            element.style.display = "none";
+        }
+        else{
+            element.style.display = "flex";
+        }
+    }
+    /*
+    keyWhich.textContent = keyObject.keyCode;  DEPRECATED 
+    key.charCodeAt() is the replacement, but is case sensitive
+    Converting it to upper case makes it match the "keyCode" value
+    */
+    let which = keyObject.key.toUpperCase().charCodeAt();
+    keyWhich.textContent = which;
+    
+    keyPressed.textContent = keyObject.key;
+    keyCode.textContent = keyObject.code;
+
+    mainMovement.restart();
+
+    anime({
+        targets: keyMain,
+        innerText: [0, which],
+        easing: "linear",
+        duration: 500,
+        round: true,
+        update: function(a) {
+            keyMain.innerHTML = a.animations[0].currentValue;
+        }
+    });
+
+    //anime(mainTextChange);
+});
+
